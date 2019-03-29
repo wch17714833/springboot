6438719e3552ca3edd339c654ecb4bf5bd0b9284
wench7 <wench7@chinaunicom.cn>
2019-03-29 09:55:41 +0800

    新增cas client 客户端代码 tomcate试用8 7严重报错

--- src/main/java/com/wench/web/WebApplication.java
+++ src/main/java/com/wench/web/WebApplication.java
@@ -1,12 +1,34 @@
 package com.wench.web;
 
+import net.unicon.cas.client.configuration.EnableCasClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
-public class WebApplication {
+@EnableCasClient//启用cas client
+public class WebApplication extends SpringBootServletInitializer {
+    private Logger logger = LoggerFactory.getLogger(WebApplication.class);
+    @RequestMapping("/index")
+    public String index(ModelMap map) {
+        System.out.println("111111111111111111111111WWW");
+
+        map.addAttribute("name", "clien B");
+        return "index";
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WebApplication.class);
+    }
 
     public static void main(String[] args) {
+        System.out.println("222");
+
         SpringApplication.run(WebApplication.class, args);
     }
 
---
+++ src/main/java/com/wench/web/controller/CASController.java
@@ -0,0 +1,23 @@
+package com.wench.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/cas")
+public class CASController {
+    @RequestMapping("/hello")
+    public ModelAndView hello() {
+        System.out.println("跳转路径");
+        ModelAndView modelAndView = new ModelAndView("/home/login");
+        modelAndView.addObject("welcome","123");
+        return modelAndView;
+    }
+    @RequestMapping("/index")
+    public String index(ModelMap map) {
+        map.addAttribute("name", "clien B");
+        return "index";
+    }
+}
